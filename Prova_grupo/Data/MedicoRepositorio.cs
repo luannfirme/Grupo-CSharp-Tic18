1e2ef3e891fe28cdf528e51d91ef085e1d60b447
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-12-04 16:35:29 -0300

    feature: classes, atributos, e metodos

---
+++ Prova_grupo/Data/MedicoRepositorio.cs
@@ -0,0 +1,56 @@
+using Prova_grupo.Domain;
+
+namespace Prova_grupo.Data{
+    public class MedicoRepositorio : PessoaRepoitorio
+    {
+        private List<Medico> medicoList = new List<Medico>();
+        public override void CadastrarMedico(Medico medico){
+            base.CadastrarMedico(medico);
+
+            medicoList.Add(medico);
+        }
+
+        public List<Medico> ListarTodos(){
+            return medicoList;
+        }
+
+        public override int TamLista(){
+            base.TamLista();
+            return medicoList.Count;
+        }
+        public bool VerificaCRM(string CRM){
+            foreach (var medico in medicoList) {
+                if(medico.CRM == CRM){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public override bool VerificaCPF(string CPF){
+            base.ValidaCPF(CPF);
+            foreach (var medico in medicoList) {
+                if(medico.CPF == CPF){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public List<Medico> GerarRelatorioIdadeMinMaxMedico(int idadeMinimo, int idadeMaximo){
+            return medicoList.FindAll(m => m.IdadePessoa >= idadeMinimo && m.IdadePessoa <= idadeMaximo);
+        }
+        public Medico BuscaPorId(int id){
+            var medicoId = medicoList.Find(m=>m.Id == id);
+            if(medicoId != null){
+                return medicoId;
+            }else{
+                throw new InvalidOperationException($"Medico com ID {id} não encontrado");
+            }
+        }
+
+
+    }
+    
+
+}
---
+++ Prova_grupo/Data/PacienteRepositorio.cs
@@ -0,0 +1,111 @@
+using System.Runtime.Serialization;
+using Prova_grupo.Domain;
+
+namespace Prova_grupo.Data
+{
+    public class PacienteRepositorio: PessoaRepoitorio
+    {
+        private List<Paciente> pacienteList = new List<Paciente>();
+        private List<string> sintomas = new List<string> ();
+
+        public override void CadastrarPaciente(Paciente paciente)
+        {
+            base.CadastrarPaciente(paciente);
+            pacienteList.Add(paciente);
+
+        }
+
+        public void AddSintomas(string sintoma){
+            sintomas.Add(sintoma);
+        }
+
+        public List<Paciente> ListaTodosPacientes(){
+            return pacienteList;
+        }
+
+        public override int TamLista()
+        {
+            base.TamLista();
+            return pacienteList.Count;
+        }
+
+        public override bool VerificaCPF(string CPF){
+            base.ValidaCPF(CPF);
+            foreach (var paciente in pacienteList) {
+                if(paciente.CPF == CPF){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public List<Paciente> GerarRelatorioIdadeMinMaxPaciente(int idadeMinimo, int idadeMaximo){
+            var buscaMinMax = pacienteList.FindAll(m => m.IdadePessoa >= idadeMinimo && m.IdadePessoa <= idadeMaximo);
+
+            if(buscaMinMax != null){
+                return buscaMinMax;
+            }else{
+                throw new InvalidOperationException($"Paciente com idade entre {idadeMinimo} e {idadeMaximo} não encontrado");
+            }       
+        }
+
+        public List<Paciente> BuscarPacientesPeloSexo(string sexo){
+            var buscaSexo = pacienteList.FindAll(m => m.Sexo == sexo);
+
+            if(buscaSexo  != null){
+                return buscaSexo ;
+            }else{
+                throw new InvalidOperationException($"Pacientes do sexo {sexo} não encontrado");
+            } 
+            
+        }
+
+        public List<Paciente> ImprimiPacienteOrdemAlfabetica(){
+            
+            var pacienteOrdenado =  pacienteList.OrderBy(p => p.Nome).ToList();
+            if(pacienteOrdenado  != null){
+                return pacienteOrdenado ;
+            }else{
+                throw new InvalidOperationException($"Listas de Pacientes vazia");
+            } 
+            
+        }
+
+        public List<Paciente> BuscaPacienteSintomas(string sintoma){
+            
+            var buscaSintomas =  pacienteList.FindAll(p => p.Sintomas.Contains(sintoma));
+            if(buscaSintomas  != null){
+                return buscaSintomas ;
+            }else{
+                throw new InvalidOperationException($"Pacientes com sintoma {sintoma} não encontrado");
+            }            
+        }
+
+        public Paciente BuscaPaciPorId(int id){
+            
+            var buscaID = pacienteList.Find(p=>p.IdPaciente == id);
+            if(buscaID  != null){
+                return buscaID ;
+            }else{
+                throw new InvalidOperationException($"Pacientes com id {id} não encontrado");
+            }                        
+        }
+
+        public List<Paciente>  ImprimiPorMesNscimento(int mes) {
+            var pacientesOrdenadosMesNasci = pacienteList.OrderBy(p => p.DataNascimento.Month == mes).ToList();
+
+            if(pacientesOrdenadosMesNasci  != null){
+                return pacientesOrdenadosMesNasci;
+            }else{
+                throw new InvalidOperationException($"Pacientes do {mes} não encontrado");
+            } 
+
+        // foreach (var paciente in pacientesOrdenadosMesNasci) {
+        //     Console.WriteLine($"--Paciente--: \nCPF: {paciente.CPF}, \nNome: {paciente.Nome}, \nDataNasc: {paciente.DataNascimento}, \nSexo: {paciente.Sexo}, \nSintomas: {paciente.Sintomas}, \n");
+        // }    
+}
+        
+
+
+    }
+}
