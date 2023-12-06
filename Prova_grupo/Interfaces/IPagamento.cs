26d5912a7c2c08459d79b852425ce5bf4f04397c
luannfirme <luannfirme@gmail.com>
2023-12-06 12:52:22 -0300

    atualização

--- Prova_grupo/Interfaces/IPagamento.cs
+++ Prova_grupo/Interfaces/IPagamento.cs
@@ -1,15 +1,9 @@
-﻿namespace Prova_grupo.Domain
-{
-    public class Pagamento
-    {
-        public readonly string TipoCartao = "Cartão de crédito";
-        public readonly string TipoBoleto = "Boleto Bancário";
-        public readonly string TipoDinheiro = "Dinheiro em Espécie";
+﻿using Prova_grupo.Domain;
 
-        public string Descricao { get; set; }
-        public double Valor { get; set; }
-        public double Desconto { get; set; }
-        public DateTime Data { get; set; }
-        public string Tipo { get; set; }
+namespace Prova_grupo.Interfaces
+{
+    internal interface IPagamento
+    {
+        public void RealizarPagamento(int id, Pagamento pagamento);
     }
 }
