d9cd5ae30e12f5a92692d11f3d228ed1cd57b617
luannfirme <luannfirme@gmail.com>
2023-12-06 12:45:10 -0300

    parte individual

--- Prova_grupo/Program.cs
+++ Prova_grupo/Program.cs
@@ -1,18 +1,21 @@
 ﻿using Prova_grupo.Service;
 
 using Prova_grupo.Apresentacao;
-namespace Prova_grupo{
-    class Program{
-        static void Main(){
+namespace Prova_grupo
+{
+    class Program
+    {
+        static void Main()
+        {
             MedicoPacienteApresentacao medicoPacienteApresentacao = new MedicoPacienteApresentacao();
 
             MedicoService medicoService = new MedicoService();
             PacienteService pacienteService = new PacienteService();
+            PlanoDeSaudeService planoDeSaudeService = new PlanoDeSaudeService();
+            AtendimentoService atendimentoService = new AtendimentoService();
 
+            medicoPacienteApresentacao.MenuDeServicos(medicoService, pacienteService, planoDeSaudeService, atendimentoService);
 
-            medicoPacienteApresentacao.MenuDeServicos(medicoService, pacienteService);
-
- 
         }
     }
 }
