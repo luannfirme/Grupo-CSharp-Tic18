e583909c6af9b70746625dc39d8900643269d772
MarcosVTG <147275008+MarcosVTG@users.noreply.github.com>
2023-12-06 12:29:15 -0300

    Update PacienteRepositorio.cs

--- Prova_grupo/Data/PacienteRepositorio.cs
+++ Prova_grupo/Data/PacienteRepositorio.cs
@@ -1,9 +1,9 @@
-using System.Runtime.Serialization;
 using Prova_grupo.Domain;
+using Prova_grupo.Interfaces;
 
 namespace Prova_grupo.Data
 {
-    public class PacienteRepositorio: PessoaRepoitorio
+    public class PacienteRepositorio: PessoaRepoitorio, IPagamento
     {
         private List<Paciente> pacienteList = new List<Paciente>();
         private List<string> sintomas = new List<string> ();
@@ -91,6 +91,16 @@ namespace Prova_grupo.Data
             }                        
         }
 
+        public Paciente BuscaPaciPorCpf(string cpf){
+            
+            var buscaID = pacienteList.Find(p=>p.CPF == cpf);
+            if(buscaID  != null){
+                return buscaID ;
+            }else{
+                throw new InvalidOperationException($"Pacientes com CPF {cpf} não encontrado");
+            }                        
+        }
+
         public List<Paciente>  ImprimiPorMesNscimento(int mes) {
             var pacientesOrdenadosMesNasci = pacienteList.OrderBy(p => p.DataNascimento.Month == mes).ToList();
 
@@ -99,8 +109,26 @@ namespace Prova_grupo.Data
             }else{
                 throw new InvalidOperationException($"Pacientes do {mes} não encontrado");
             }  
-        }        
+        }
 
+        public void RealizarPagamento(int id, Pagamento pagamento)
+        {
+            var paciente = pacienteList.FirstOrDefault(p => p.IdPaciente == id);
+            if(paciente  == null){
+                throw new InvalidOperationException($"Pacientes com ID: {id} não encontrado");
+            }else{
+                paciente.Pagamentos.Add(pagamento);
+            }
+        }
 
+        public void addPlano(int id, PlanoDeSaude plano)
+        {
+            var paciente = pacienteList.FirstOrDefault(p => p.IdPaciente == id);
+            if(paciente  == null || plano == null){ 
+                throw new InvalidOperationException($"Pacientes com ID: {id} não encontrado");
+            }else{
+                paciente.PlanoPaciente = plano;
+            }
+        }
     }
 }
