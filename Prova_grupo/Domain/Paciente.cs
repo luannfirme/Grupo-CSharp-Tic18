692f7be1158dfcba41e51f50816d1829522771b8
luannfirme <luannfirme@gmail.com>
2023-12-06 12:54:28 -0300

    finalização

--- Prova_grupo/Domain/Paciente.cs
+++ Prova_grupo/Domain/Paciente.cs
@@ -3,9 +3,11 @@ namespace Prova_grupo.Domain{
 
     public int IdPaciente {get; set; }
     public string Sexo{get; set;}
-    
+    public PlanoDeSaude PlanoPaciente { get; set; }
     public List<string> Sintomas{get; set;}
-    public Paciente(int idPaciente, string nome, DateTime dataNascimento, string cpf, string sexo, List<string> sintomas)
+    public List<Pagamento> Pagamentos { get; set; } = new List<Pagamento>();
+
+        public Paciente(int idPaciente, string nome, DateTime dataNascimento, string cpf, string sexo, List<string> sintomas)
             : base(nome, dataNascimento, cpf){
                 
             if (string.IsNullOrWhiteSpace(cpf)){
